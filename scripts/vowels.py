b9366104d8965ac8f9eb4c7695c442cbe14dab0e
Joshua Adebayo <85533331+Epicskylegend@users.noreply.github.com>
2022-08-16 21:49:48 -0500

    Update vowels.py

--- scripts/vowels.py
+++ scripts/vowels.py
@@ -1,1 +1,10 @@
-# Write the solution for "Filter Vowels".
+from distutils.filelist import findall
+
+
+import re
+class filterVowels:
+   vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+re.findall(vowels in vowels.txt) # find all the instances of vowels inside vowels.txt which we defined above.
+
+print("vowels-" + vowels + ".txt") # print the amount of vowels found in the format given from the problem.
