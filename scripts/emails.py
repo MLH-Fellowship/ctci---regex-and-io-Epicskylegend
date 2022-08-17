621622fcf3e9038dcc8550f2131c52d538d3f95d
Joshua Adebayo <85533331+Epicskylegend@users.noreply.github.com>
2022-08-16 21:48:51 -0500

    Update emails.py

--- scripts/emails.py
+++ scripts/emails.py
@@ -1,1 +1,12 @@
-# Write the solution for "New Emails".
+from ast import pattern
+import re
+class example1: 
+
+    pattern = ([A-Z] + '@' + 'gmail.com') # Pattern that emails inside the email file follow.
+
+  
+    result = re.sub(pattern, '','@mlh.io') in emails.csv # changes the "@gmail.com" section to "@mlh.io"
+  
+    
+    print(pattern + result) in newfile # prints the end product which is the name of the email plus the "@mlh.io" section as a single string inside a new file.
+   
