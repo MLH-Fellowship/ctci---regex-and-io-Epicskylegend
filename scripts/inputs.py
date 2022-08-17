04c46866e01f7cedbeebc27ac95baa358e443b9b
Joshua Adebayo <85533331+Epicskylegend@users.noreply.github.com>
2022-08-16 21:49:10 -0500

    Update inputs.py

--- scripts/inputs.py
+++ scripts/inputs.py
@@ -1,1 +1,13 @@
-# Write the solution for "Split the files".
+import os.path
+import re
+
+class splitTheFiles:
+
+    file_path1 = r' C:\Users\adeba\OneDrive\MLH\CTCI\split\github.txt'
+    file_path2 = r' C:\Users\adeba\OneDrive\MLH\CTCI\split\meta.txt' #size of the files in bytes
+    sz = os.path.getsize(file_path1 and file_path2)
+
+    if (getsize(file_path1 or file_path2) > 500)
+    # splitlines in half for both files.
+    else #keep files the same
+     
