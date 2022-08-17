459ff8d5be353935275b223c489935ea3bd638b7
Joshua Adebayo <85533331+Epicskylegend@users.noreply.github.com>
2022-08-16 21:49:31 -0500

    Update longest.py

--- scripts/longest.py
+++ scripts/longest.py
@@ -1,1 +1,8 @@
-# Write the solution for "Longest Word".
+class longestWord:
+
+    def find_longest_word(word_list): 
+
+        f = open(longest.txt)
+        longest_word =  max(word_list, key=len)
+        return longest_word # returns the longest word
+        (color.BOLD + longest_word + color.END) # makes color bold for all occurences of the longest word.
